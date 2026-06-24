54b2fef63e88a094f71b7ce6dd8ee503f97127b4
MyhailoCherkasov <m.cherkasov@student.bachelor-goit.com.ua>
2026-06-24 16:26:17 +0300

    Add schemas for contact management

---
+++ schemas.py
@@ -0,0 +1,27 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+
+class ContactBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone: str
+    birthday: date
+    additional_data: Optional[str] = None
+
+
+class ContactCreate(ContactBase):
+    pass
+
+
+class ContactUpdate(ContactBase):
+    pass
+
+
+class ContactResponse(ContactBase):
+    id: int
+
+    class Config:
+        from_attributes = True
